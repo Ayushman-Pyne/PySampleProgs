7ec2fb8ffcccbca121059a6b6017361349f1e875
Ayushman Pyne <ayushman.pyne@gmail.com>
2025-04-13 19:19:07 +0000

    update crechk py

--- crechk.py
+++ crechk.py
@@ -8,10 +8,35 @@
 
 # Step 1
 
+credit_card = "4111111111111111"
+credit_card = credit_card.replace("-","")
+credit_card = credit_card.replace(" ","")
+credit_card = credit_card[::-1]
+
+
 # Step 2
+
+odd_sum = 0
+for x in credit_card[::2]:
+    odd_sum += int(x)
 
 # Step 3
 
+even_sum = 0
+for x in credit_card[1::2]:
+    x = int(x) * 2
+    if x > 10:
+        even_sum += (1 + x%10)
+    else:
+        even_sum += x
+
 # Step 4
 
+tot_sum = even_sum + odd_sum
+
 # Step 5
+
+if(tot_sum%10 == 0):
+    print("Valid")
+else:
+    print("Not Valid")
