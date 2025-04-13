e67d2c86c61061a1a1fa6e5f36c015ee9ab4ed21
Ayushman Pyne <ayushman.pyne@gmail.com>
2025-04-13 19:00:50 +0000

    1st prog com

---
+++ crechk.py
@@ -0,0 +1,17 @@
+# Credit card Validator program(Beginner)
+# Step 1 - Remove any '-' or ' '
+# Step 2 - Add all digits in the odd places from right to left
+# Step 3 - Double Every second digit from right to left
+#           (If result is a two digit number , add the two digit number together to get a single digit)
+# Step 4 - Sum of totals from Step 2 and 3
+# Step 5 - If Sum is divisible by 10, the credit card number is valid.
+
+# Step 1
+
+# Step 2
+
+# Step 3
+
+# Step 4
+
+# Step 5
